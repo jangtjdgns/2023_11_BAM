f7a78a9ddc80c6b83198bfebccc9df1c5bfc5016
jangtjdgns <jangtjdgns0208@gamil.com>
2023-11-14 20:20:01 +0900

    memberController 생성

--- src/example/App.java
+++ src/example/App.java
@@ -4,27 +4,26 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
+import example.controller.MemberController;
 import example.dto.Article;
-import example.dto.Member;
 import example.util.Util;
 
 public class App {
 	private int articleId;
-	private int memberId;
 	private List<Article> articles;
-	private List<Member> members;
+	
 
 	App() {
 		this.articleId = 0;
-		this.memberId = 0;
 		this.articles = new ArrayList<>();
-		this.members = new ArrayList<>();
 	}
 
 	void run() {
 		System.out.println("== 시작 ==");
 
 		Scanner sc = new Scanner(System.in);
+		
+		MemberController memberController = new MemberController(sc);
 
 		addTestArticle();
 
@@ -40,77 +39,10 @@ public class App {
 			if (cmd.equals("exit")) {
 				break;
 			}
-
 			
-			
-			// 회원가입
 			if (cmd.equals("member join")) {
-				
-				String loginId = null;
-				while(true) {
-					System.out.printf("아이디: ");
-					loginId = sc.nextLine().trim();
-					
-					if(loginId.length() == 0) {
-						System.out.println("아이디는 필수 입력정보입니다.");
-						continue;
-					}
-					
-					boolean loginIdDupChk = false;
-					
-					for(Member member : this.members) {
-						if(member.loginId.equals(loginId)) {
-							loginIdDupChk = true;
-							break;
-						}
-					}
-					
-					if(loginIdDupChk) {
-						System.out.printf("%s은(는) 이미 사용중인 아이디입니다.\n", loginId);
-						continue;
-					}
-					
-					System.out.printf("%s은(는) 사용가능한 아이디입니다.\n", loginId);
-					break;
-				}
-				
-				
-				String loginPw = null;
-				while(true) {
-					System.out.printf("비밀번호: ");
-					loginPw = sc.nextLine().trim();
-					
-					if(loginPw.length() == 0) {
-						System.out.println("비밀번호는 필수 입력정보입니다.");
-						continue;
-					}
-					
-					System.out.printf("비밀번호 확인: ");
-					String loginPwChk = sc.nextLine().trim();
-					
-					if(!loginPw.equals(loginPwChk)) {
-						System.out.println("비밀번호를 다시 입력해주세요.");
-						continue;
-					}
-					break;
-				}
-
-				String userName = null;
-				while(true) {
-					System.out.printf("이름: ");
-					userName = sc.nextLine().trim();
-					
-					if(userName.length() == 0) {
-						System.out.println("이름은 필수 입력정보입니다.");
-						continue;
-					}
-					break;
-				}
-				
-				Member member = new Member(++memberId, Util.getDateStr(), loginId, loginPw, userName);
-				members.add(member);
+				memberController.doJoin();
 			}
-
 			// 게시물 작성
 			else if (cmd.equals("article write")) {
 				articleId++;
---
+++ src/example/controller/MemberController.java
@@ -0,0 +1,88 @@
+package example.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+import example.dto.Member;
+import example.util.Util;
+
+public class MemberController {
+	private int memberId;
+	private List<Member> members;
+	Scanner sc;
+
+	public MemberController(Scanner sc) {
+		this.memberId = 0;
+		this.members = new ArrayList<>();
+		this.sc = sc;
+	}
+
+	// 회원가입
+	public void doJoin() {
+		String loginId = null;
+		while (true) {
+			System.out.printf("아이디: ");
+			loginId = sc.nextLine().trim();
+
+			if (loginId.length() == 0) {
+				System.out.println("아이디는 필수 입력정보입니다.");
+				continue;
+			}
+
+			boolean loginIdDupChk = false;
+
+			for (Member member : this.members) {
+				if (member.loginId.equals(loginId)) {
+					loginIdDupChk = true;
+					break;
+				}
+			}
+
+			if (loginIdDupChk) {
+				System.out.printf("%s은(는) 이미 사용중인 아이디입니다.\n", loginId);
+				continue;
+			}
+
+			System.out.printf("%s은(는) 사용가능한 아이디입니다.\n", loginId);
+			break;
+		}
+
+		String loginPw = null;
+		while (true) {
+			System.out.printf("비밀번호: ");
+			loginPw = sc.nextLine().trim();
+
+			if (loginPw.length() == 0) {
+				System.out.println("비밀번호는 필수 입력정보입니다.");
+				continue;
+			}
+
+			System.out.printf("비밀번호 확인: ");
+			String loginPwChk = sc.nextLine().trim();
+
+			if (!loginPw.equals(loginPwChk)) {
+				System.out.println("비밀번호를 다시 입력해주세요.");
+				continue;
+			}
+			break;
+		}
+
+		String userName = null;
+		while (true) {
+			System.out.printf("이름: ");
+			userName = sc.nextLine().trim();
+
+			if (userName.length() == 0) {
+				System.out.println("이름은 필수 입력정보입니다.");
+				continue;
+			}
+			break;
+		}
+
+		Member member = new Member(++memberId, Util.getDateStr(), loginId, loginPw, userName);
+		members.add(member);
+
+		System.out.println("회원가입 되었습니다.");
+	}
+}
